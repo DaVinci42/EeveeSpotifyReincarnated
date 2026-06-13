586fcdf3cd6fc85af178ddfe4fd12fe2a4e8dfdd
Mod4 <omarelfarok24135@gmail.com>
2026-06-13 17:43:17 +0400

    Update EeveeContributor.swift

--- Sources/EeveeSpotify/Settings/Shared/GitHub/Models/EeveeContributor.swift
+++ Sources/EeveeSpotify/Settings/Shared/GitHub/Models/EeveeContributor.swift
@@ -2,4 +2,10 @@ struct EeveeContributor: Decodable, Equatable {
     var usernames: [String]
     var displayName: String?
     var roles: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case usernames
+        case displayName
+        case roles
+    }
 }
